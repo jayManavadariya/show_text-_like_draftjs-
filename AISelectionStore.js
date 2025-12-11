0b87f45670ba9d03bd57871972afefd6b26cf8c1
jayManavadariya <jaymanavadariyamcg@gmail.com>
2025-12-11 14:21:04 +0530

    Update AISelectionStore.js

--- AISelectionStore.js
+++ AISelectionStore.js
@@ -2,15 +2,16 @@ import { create } from 'zustand';
 
 export const useAISelectionStore = create((set) => ({
   selectedValue: null,
-  selectedEditorState: null,
+  selectedDisplayHTML: null,
+  selectionList: [],
   aiState: {
     selectedTitle: null,
     inputValue: '',
     loading: false,
     aiOutPut: null,
   },
-  setSelectedValue: (selectedValue) => set({ selectedValue }),
-  setSelectedEditorState: (selectedEditorState) => set({selectedEditorState}),
+  setSelectedValue: (selectedValue, selectedDisplayHTML = null, selectionList = []) =>
+    set({ selectedValue, selectedDisplayHTML, selectionList }),
   updateAIState: (partialState) =>
     set((state) => ({
       aiState: { ...state.aiState, ...partialState },
